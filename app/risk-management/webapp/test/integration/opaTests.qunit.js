5029aa41c123630a819bd3c6c174ee5a943b3a11
Marcelo Mercado <mercadomarcelorlz@gmail.com>
2024-01-08 20:57:46 +0000

    Initial Commit

---
+++ app/risk-management/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'rm/riskmanagement/test/integration/FirstJourney',
+		'rm/riskmanagement/test/integration/pages/RisksList',
+		'rm/riskmanagement/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('rm/riskmanagement') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
